df5144606f78fc60b9d12dd8b4b22d3da96b2859
KHU MINI <jsm6858@gmail.com>
2026-02-12 12:18:23 +0900

    Refactor speed conversion functions with comments

--- 속도변환기.py
+++ 속도변환기.py
@@ -1,10 +1,9 @@
-def Km_to_meter(x):
+def Km_to_meter(x): #Km/h를 m/s로 변환
     m = x * 10/36
     print(f"{x}Km/h는 {m}m/s입니다.")
 
-def meter_to_Km(y):
+def meter_to_Km(y): #m/s를 Km/h로 변환
     k = y * 36/10
     print(f"{y}m/s는 {k}Km/h입니다.")
 
-Km_to_meter(440)
-meter_to_Km(30)
+
