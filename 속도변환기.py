3b19f10a97bdb2492cfde8e42d850286a686c91d
KHU MINI <jsm6858@gmail.com>
2026-02-12 12:12:29 +0900

    Add files via upload

---
+++ 속도변환기.py
@@ -0,0 +1,10 @@
+def Km_to_meter(x):
+    m = x * 10/36
+    print(f"{x}Km/h는 {m}m/s입니다.")
+
+def meter_to_Km(y):
+    k = y * 36/10
+    print(f"{y}m/s는 {k}Km/h입니다.")
+
+Km_to_meter(440)
+meter_to_Km(30)
